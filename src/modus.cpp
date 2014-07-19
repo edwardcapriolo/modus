87ffe949724059cad8a9c701f8e7c68df9743782
Edward Capriolo <edlinuxguru@gmail.com>
2014-07-18 21:05:46 -0400

    First commit

---
+++ src/modus.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : modus.cpp
+// Author      : Edward Capriolo
+// Version     :
+// Copyright   : Apache V2
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello modus!!!" << endl; // prints !!!Hello modus!!!
+	return 0;
+}
