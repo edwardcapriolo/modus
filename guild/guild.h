aeed6638b406090629e9cb0aca6854736f89e0fb
Edward Capriolo <edlinuxguru@gmail.com>
2014-08-07 23:22:33 -0400

    Changes

---
+++ guild/guild.h
@@ -0,0 +1,61 @@
+/*
+ * guild.h
+ *
+ *  Created on: Jul 23, 2014
+ *      Author: edward
+ */
+
+#ifndef GUILD_H_
+#define GUILD_H_
+#include <iostream>
+#include <etcdcpp/libetcdcpp.cxx>
+
+using namespace std;
+using namespace etcdcpp;
+
+namespace modus {
+
+class actor_info {
+public:
+  string host;
+  unsigned short port;
+};
+
+
+/*
+ * A registry for actors backed by etcd
+ */
+class guild {
+
+public:
+  const string base_path = "/actors";
+
+  guild(etcd_session * session, int expire_in_seconds) {
+    this->expire_in_seconds = expire_in_seconds;
+    this->session = session;
+  }
+
+  void register_actor (string actor_name, string actor_host, int actor_port){
+    string path_to_actor_list = base_path + "/" + actor_name + "/" + actor_host + ":" +
+    (static_cast<ostringstream*>( &(ostringstream() << actor_port) )->str());
+    Document * result = session->set(path_to_actor_list, "", expire_in_seconds);
+    //detect error
+    delete result;
+  }
+
+  vector<actor_info> search_actors(string actor_name){
+    string path_to_actor_list = base_path + "/" + actor_name;
+    Document * results = session->get(path_to_actor_list);
+    //parse results
+    delete results;
+    vector <actor_info> res;
+    return res;
+  }
+
+protected:
+  etcd_session * session;
+  int expire_in_seconds;
+};
+
+} //end modus namespace
+#endif /* GUILD_H_ */
--- guild/guild_reporter.cpp
+++ guild/guild_reporter.cpp
@@ -23,44 +23,36 @@ namespace modus {
 
 //Put this here so it is free fx
 
-void register_actor(event_based_actor * );
+void register_actor(event_based_actor *);
+
 class guild_reporter {
 
 public:
-  guild_reporter(guild * guild_member, int report_seconds, string report_host, int report_port){
+  guild_reporter(guild * guild_member, int report_seconds){
     this->guild_member = guild_member;
-    this->report_seconds = report_seconds;
-    this->host = report_host;
-    this->port = report_port;
-     x = spawn(register_actor);
+    x = spawn(register_actor );
   }
 
   void register_actor_instance(actor a, string name){
-	this->actors.push_back(a);
-    this->names.push_back(name);
   }
-
 
 private:
 
-  vector<actor> actors;
-  vector<string> names;
   guild * guild_member;
-  string host;
-  int port;
   int report_seconds;
   actor x;
 };
 
 void register_actor(event_based_actor* self){
-	  self->send(self, atom("modus-tick"));
-	  self->become (
-		on(atom("modus-tick")) >> [=] () {
-		  cout << "!!!tic!!!" << endl; // prints !!!Hello modus!!!
-		  self->delayed_send(self, std::chrono::milliseconds(5000), atom("modus-tick") );
-		 }
-	   );
-	 }
+  self->send(self, atom("modus-tick"));
+  self->become (
+    on(atom("modus-tick")) >> [=] () {
+      cout << "!!!tic!!!" << endl; // prints !!!Hello modus!!!
+      self->delayed_send(self, std::chrono::milliseconds(5000), atom("modus-tick") );
+    }
+);
+
+}
 
 }
 
